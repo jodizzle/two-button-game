2d7915ca0dc0d5aa39c640523a99ee92c9da03de
Jody Leonard <me@jodyleonard.com>
2015-12-13 22:53:01 -0500

    Make variables lowercase

--- Source/Main.hx
+++ Source/Main.hx
@@ -10,10 +10,10 @@ import openfl.Assets;
 import Math;
 
 class Main extends Sprite {
-	private var Square:Sprite;
-	private var Square2:Sprite;
-	private var Image:Bitmap;
-	private var Image2:Bitmap;
+	private var square:Sprite;
+	private var square2:Sprite;
+	private var image:Bitmap;
+	private var image2:Bitmap;
 	private var gameContainer:Sprite;
 
 	private var left:Bool;
@@ -25,28 +25,28 @@ class Main extends Sprite {
 	public function new() {
 		super();
 
-		Square = new Sprite();
-		Square.x = stage.stageWidth/2;
-		Square.y = stage.stageHeight/2;
-		Square2 = new Sprite();
-		Square2.x = 100;
-		Square2.y = 100;
+		square = new Sprite();
+		square.x = stage.stageWidth/2;
+		square.y = stage.stageHeight/2;
+		square2 = new Sprite();
+		square2.x = 100;
+		square2.y = 100;
 
-		Image = new Bitmap(Assets.getBitmapData("assets/sprite_multicolor.png"));
-		Image.x = -(Image.width)/2;
-		Image.y = -(Image.height)/2;
-		Image2 = new Bitmap(Assets.getBitmapData("assets/sprite.png"));
-		Image.x = -(Image2.width)/2;
-		Image.y = -(Image2.height)/2;
+		image = new Bitmap(Assets.getBitmapData("assets/sprite_multicolor.png"));
+		image.x = -(image.width)/2;
+		image.y = -(image.height)/2;
+		image2 = new Bitmap(Assets.getBitmapData("assets/sprite.png"));
+		image.x = -(image2.width)/2;
+		image.y = -(image2.height)/2;
 
 		//All game objects with become children of gameContainer, which will then become
 		//a child of Main
 		gameContainer = new Sprite();
 
-		Square.addChild(Image);
-		Square2.addChild(Image2);
-		gameContainer.addChild(Square);
-		gameContainer.addChild(Square2);
+		square.addChild(image);
+		square2.addChild(image2);
+		gameContainer.addChild(square);
+		gameContainer.addChild(square2);
 		addChild(gameContainer);
 
 		stage.addEventListener(KeyboardEvent.KEY_DOWN, stage_onKeyDown);
@@ -73,19 +73,19 @@ class Main extends Sprite {
 
 	private function this_onEnterFrame(event:Event):Void {
 		if(left) {
-			Square.rotation += -1*rotationSpeed;
+			square.rotation += -1*rotationSpeed;
 		}
 		if(right) {
-			Square.rotation += rotationSpeed;
+			square.rotation += rotationSpeed;
 		}
 
 		//displayObjectContainer.rotation is in degrees, need to convert to radians for Math.sin and Math.cos
-		var radians:Float = Square.rotation*(Math.PI/180);
-		Square.y += moveSpeed*Math.sin(radians);
-		Square.x += moveSpeed*Math.cos(radians);
+		var radians:Float = square.rotation*(Math.PI/180);
+		square.y += moveSpeed*Math.sin(radians);
+		square.x += moveSpeed*Math.cos(radians);
 
-		//Transform gameContainer so that it offsets Square transformations and keeps Square centered on the stage
-		gameContainer.x = -Square.x + stage.stageWidth/2;
-		gameContainer.y = -Square.y + stage.stageHeight/2;
+		//Transform gameContainer so that it offsets square transformations and keeps square centered on the stage
+		gameContainer.x = -square.x + stage.stageWidth/2;
+		gameContainer.y = -square.y + stage.stageHeight/2;
 	}
 }
